de174fb045aa9ea6bad5423e7e778bd374efe93b
skoplev <skoplev@gmail.com>
2016-11-23 20:36:27 -0500

    Improved plots interpreting WGCNA cross-tissue modules.

--- co-expression/moduleStats.R
+++ co-expression/moduleStats.R
@@ -1,5 +1,7 @@
 library(reshape2)
 library(RColorBrewer)
+library(gplots)
+
 
 # Load gwnet and row_meta tables
 load(file.path(data_dir, "modules/cross-tissue.RData"))
@@ -52,23 +54,36 @@ entropy = function(x) {
 # Calculate tissue entropy
 tissue_entropy = apply(module_tissue_freq, 2, entropy)
 
+# Calculate tissue frequencies
+module_tissue_freq = sweep(module_tissue, 2, apply(module_tissue, 2, sum), "/")
 
-min_entro = 0.3
+
+hc = hclust(dist(t(module_tissue_freq)))
+
+min_entro = 0.1
 exclude = module_size > 5000
 sel_modules = tissue_entropy > min_entro & !exclude
 
-mod_cols = rep("black", length(tissue_entropy))
-mod_cols[sel_modules] = "red"
+# Hierarcical cluster of tissue frequencies for all modules
+# Used to organize barplots
+hc_sel = hclust(dist(t(module_tissue_freq[,sel_modules])))
 
-par(mfrow=c(2, 2), lwd=1.0)
 
-plot(density(tissue_entropy), main="Module tissue entropy")
 
-# WCGNA block tissues
-barplot(block_tissue, col=tissue_col, main="K-means WCGNA blocks", ylab="Genes")
+
+svg("plots/cross-tissue-overview.svg")
+par(mfcol=c(2, 2), lwd=1.0)
+plot(density(tissue_entropy), main="Module tissue entropy")  # not used
+
+# WCGNA block tissue composition
+barplot(block_tissue, col=tissue_col,
+	main="K-means partitioned WCGNA", ylab="Genes", xlab="Blocks")
 # legend("topright", legend=rownames(block_tissue), pch=15, col=tissue_col)
 
 
+# Plot of tissue entropy and size
+mod_cols = rep("black", length(tissue_entropy))
+mod_cols[sel_modules] = "red"
 magplot(tissue_entropy, log10(module_size),
 	col=mod_cols,
 	main="Cross-tissue modules",
@@ -79,31 +94,50 @@ magplot(tissue_entropy, log10(module_size),
 
 # cross_tissue_modules = module_tissue[, order(tissue_entropy, decreasing=TRUE)[2:k]]
 cross_tissue_modules = module_tissue[, sel_modules]
-cross_tissue_modules = cross_tissue_modules[,
-	order(apply(cross_tissue_modules, 2, sum), decreasing=TRUE)]
-par(lwd=0.4)
+cross_tissue_modules = cross_tissue_modules[, hc_sel$order]  # rerder based on hclust of tissue frequencies
+
+# cross_tissue_modules = cross_tissue_modules[,
+# 	order(apply(cross_tissue_modules, 2, sum), decreasing=TRUE)]
+
+# par(lwd=0.4)
 barplot(cross_tissue_modules,
 	col=tissue_col,
-	ylab="Genes"
+	ylab="Genes",
+	xlab="Cross-tissue modules",
+	border=NA, space=0
 )
-legend("topright", legend=rownames(block_tissue), pch=22, pt.bg=tissue_col)
-
+legend("topright", legend=rownames(block_tissue),
+	pch=22,
+	pt.bg=tissue_col,
+	cex=0.7
+)
+dev.off()
 
 # Heatmap of eigengenes
-library(gplots)
 x = as.matrix(bwnet$MEs)
 
-alpha = 0.05  # eigengene color range
+
+# heatmap.2(
+# 	x,
+# 	trace="none",
+# 	col=colorRampPalette(rev(brewer.pal(9, "RdBu")))(100),
+# 	breaks=seq(-alpha, alpha, length.out=101)
+# )
+
+pdf("plots/cross-tissue-module-eigengenes.pdf", height=5)
+alpha = 0.05  # eigengene color range, +-
+x = as.matrix(bwnet$MEs)
+x = x[, sel_modules]
 heatmap.2(
 	x,
 	trace="none",
-	col=colorRampPalette(brewer.pal(9, "RdBu"))(100),
-	breaks=seq(-alpha, alpha, length.out=101)
+	col=colorRampPalette(rev(brewer.pal(9, "RdBu")))(100),
+	breaks=seq(-alpha, alpha, length.out=101)  # cap of coloring 
 )
+dev.off()
 
 
-# Calculate tissue frequencies
-# module_tissue_freq = sweep(module_tissue, 2, apply(module_tissue, 2, sum), "/")
+
 
 
 plot(frac_secondary, log10(module_size))

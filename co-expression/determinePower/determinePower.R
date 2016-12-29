b2a5c2f024386b548f5c747eba2f68b8e5b62fac
skoplev <skoplev@gmail.com>
2016-12-29 18:10:21 -0500

    Improved performance of optimizing beta values.

--- co-expression/determinePower/determinePower.R
+++ co-expression/determinePower/determinePower.R
@@ -9,6 +9,7 @@ library(reshape2)
 library(plyr)
 library(parallel)
 library(RColorBrewer)
+library(Matrix)
 
 library(compiler)
 enableJIT(3)
@@ -18,6 +19,41 @@ library(WGCNA)
 options(stringsAsFactors = FALSE)
 
 enableWGCNAThreads(nThreads=8)  # assuming 2 threads per core
+
+
+# # Filter out low connections
+# # k = cmat[cmat > 0.05]
+# # scaleFreeFitIndex(k, nBreaks=100)
+# # k = c(k1, k2)
+# fastScaleFreeFitIndex = function(k, nBreaks=50, epsilon=1e-09) {
+# 	# k = as.vector(k)
+# 	# Cut connections into discrete groups
+# 	# groups = cut(k, nBreaks)
+# 	# group_mean = tapply(k, groups, mean)  # mean connectivity in each group
+
+# 	# hist is faster than cut
+# 	groups = hist(k, breaks=nBreaks, plot=FALSE)
+
+# 	# groups = tabulate(.bincode(k, breaks=nBreaks))
+
+# 	groups$pk = groups$counts / length(k)
+# 	groups$log_pk = log(groups$pk + epsilon)
+# 	groups$log_mids = log(groups$mids)
+
+# 	# Count number of connections in each group
+# 	# group_pk = table(groups) / length(k)
+
+# 	# fit = fastLm(log(group_pk)~log(group_mean))
+# 	# fit = lm(log(groups$pk) ~ log(groups$mids))
+
+# 	fit = lm(groups$log_pk ~ groups$log_mids)
+
+# 	out = data.frame(Rsquared.SFT=summary(fit)$r.squared)
+# 	return(out)
+
+# 	plot(groups$log_mids, groups$log_pk)
+# }
+
 
 setwd("/Users/sk/Google Drive/projects/cross-tissue")
 source("src/base.R")
@@ -37,6 +73,10 @@ pheno = fread(file.path(
 # Load imputed recast gene expression matrix
 load(file.path(data_dir, "STARNET/gene_exp_norm_batch_imp/all.RData"), verbose=TRUE)
 
+if (any(is.na(mat))) {
+	stop("Gene expression matrix contains missing data.")
+}
+
 # Standardize expression data
 # mat_scaled = scale(t(mat))
 
@@ -44,45 +84,126 @@ load(file.path(data_dir, "STARNET/gene_exp_norm_batch_imp/all.RData"), verbose=T
 pheno_matched = pheno[match(colnames(mat), pheno$starnet.ID), ]
 
 # Picking a soft power threshold to get scale-free correlation networks from each tissue alone
+powers = seq(1, 10, length.out=20)
 # powers = seq(1, 5, length.out=50)
-powers = seq(0.1, 10, length.out=50)
+# powers = seq(0.1, 10, length.out=50)
 block_size = 4000
 
+n_breaks = 50  # binning of connectivity
+
+# abs_cor_min = 0.05  # minimum correlation to be considered
+abs_cor_min = 0.1  # minimum correlation to be considered
+
 # Evaluate sequence of soft network cutoffs
-thresh_eval = list()
+con_eval = list()
 for (tissue in unique(row_meta$tissue)) {
 	message("Estimating power for: ", tissue)
-	submat = mat[row_meta$tissue == tissue, ]
+	# submat = mat[row_meta$tissue == tissue, ]
 
-	thresh_eval[[tissue]] = pickSoftThreshold(t(submat),
-		powerVector=powers,
-		verbose=5,
-		blockSize=block_size
+	# Symmetric correlation matrix within-tissue
+	cmat = corFast(t(mat[row_meta$tissue == tissue, ]),
+		nThreads=8  # parallel computation not working
 	)
+
+	# con_eval[[tissue]] = pickSoftThreshold.fromSimilarity(cmat,
+	# 	# t(submat),
+	# 	powerVector=powers,
+	# 	verbose=5,
+	# 	blockSize=block_size
+	# )
+
+	# Convert to adjacency matrix
+	message("making adjacency matrix")
+	cmat = abs(cmat)
+
+	# Drop entries below threshold
+	message("converting to sparse matrix")
+	cmat[cmat < abs_cor_min] = 0.0
+
+	# Convert to sparse matrix, for increased efficiency
+	cmat = Matrix(cmat, sparse=TRUE)
+	message("non-zero coefficients: ", sum(cmat > 0))
+	gc()
+
+	# Returns scale-free distribution fits and connectivity vectors for each tissue
+	# at the power series.
+	fits = lapply(powers, function(pow) {
+		message("power: ", pow)
+		message("\tadjusting weights...")
+		cmat_power = cmat^pow
+		message("\tcalculating connectivity")
+		# Calculate connectivity for nodes in both tissues
+		k = apply(cmat_power, 1, sum) - 1  # subtracting diagonal
+
+		message("\tfitting scale-free connectivity model")
+		fit = scaleFreeFitIndex(k, nBreaks=n_breaks, removeFirst=TRUE)
+		gc()
+		return(list(fit=fit, k=k))
+	})
+
+	con_eval[[i]] = fits
 }
 
+
 # Test optimal beta for combinations of tissues. Uses cross-correlations only.
-thresh_eval_pairs = list()
+con_eval_pairs = list()
 paired_tissue = combn(unique(row_meta$tissue), 2)
 for (i in 1:ncol(paired_tissue)) {
-	message("Estimating power for: ", paired_tissue[1, i], ", ", paired_tissue[2, i])
+	# message("Estimating power for: ", paired_tissue[1, i], ", ", paired_tissue[2, i])
 	# submat = mat[row_meta$tissue %in% paired_tissue[, i], ]
 
 	# Cross-correlation only
-	cmat = cor(
-		t(mat[row_meta$tissue == paired_tissue[1, i]]),
-		t(mat[row_meta_tissue == paired_tissue[2, i]])
-		use="pairwise.complete"
+	message("calculating cross-tissue correlation: ", paired_tissue[1, i], ", ", paired_tissue[2, i])
+	cmat = corFast(
+		t(mat[row_meta$tissue == paired_tissue[1, i], ]),
+		t(mat[row_meta$tissue == paired_tissue[2, i], ]),
+		nThreads=8  # might not work?
 	)
+	gc()  # garbage collection
 
-	thresh_eval_pairs[[i]] = pickSoftThreshold.fromSimilarity(
-		# t(submat),
-		cmat,
-		powerVector=powers,
-		verbose=2,
-		blockSize=block_size
-	)
+	# con_eval_pairs[[i]] = pickSoftThreshold.fromSimilarity(cmat,
+	# con_eval_pairs[[i]] = pickSoftThreshold(cmat,
+	# 	dataIsExpr=FALSE,
+	# 	# t(submat),
+	# 	powerVector=powers,
+	# 	verbose=2,
+	# 	blockSize=block_size
+	# )
+
+	# Convert to adjacency matrix
+	message("making adjacency matrix")
+	cmat = abs(cmat)
+
+	# Drop entries below threshold
+	message("converting to sparse matrix")
+	cmat[cmat < abs_cor_min] = 0.0
+
+	# Convert to sparse matrix, for increased efficiency
+	cmat = Matrix(cmat, sparse=TRUE)
+	message("non-zero coefficients: ", sum(cmat > 0))
+	gc()
+
+	# Returns scale-free distribution fits and connectivity vectors for each tissue
+	# at the power series.
+	fits = lapply(powers, function(pow) {
+		message("power: ", pow)
+		message("\tadjusting weights...")
+		cmat_power = cmat^pow
+		message("\tcalculating connectivity")
+		# Calculate connectivity for nodes in both tissues
+		k1 = apply(cmat_power, 1, sum)
+		k2 = apply(cmat_power, 2, sum)
+
+		message("\tfitting scale-free connectivity model")
+		fit = scaleFreeFitIndex(c(k1, k2), nBreaks=n_breaks, removeFirst=TRUE)
+		gc()
+		return(list(fit=fit, k1=k1, k2=k2))
+	})
+
+	con_eval_pairs[[i]] = fits
+	gc()
 }
+
 names(thresh_eval_pairs) = apply(paired_tissue, 2, paste, collapse="_")
 # thresh_eval_pairs = thresh_eval  # fix
 

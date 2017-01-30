54cd83fa4b5a4705e3b58a535f4b218131b081ff
Simon Koplev <koples01@minerva4.mgmt.hpc.mssm.edu>
2017-01-30 15:15:22 -0500

    Updated Minerva configuration for determining beta values.

--- co-expression/determinePower/run.sh
+++ co-expression/determinePower/run.sh
@@ -7,10 +7,10 @@ mkdir logs
 
 bsub -J determinePower \
 	-P acc_STARNET \
-	-q alloc \
+	-q premium \
 	-W 80:00 \
 	-n 1 \
-	-R rusage[mem=72000] \
+	-R rusage[mem=40000] \
 	-e logs/error.%J \
 	-o logs/output.%J \
 	./determinePower.R /hpc/users/koples01/links/STARNET/koples01/data/cross-tissue/gene_exp_norm_batch_imp/all.RData
